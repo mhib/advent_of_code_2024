0cffb3e633c1ff665481b0a7502f795068fc6ef2
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2024-12-20 18:02:17 +0100

    day 20

---
+++ 20/20.rb
@@ -0,0 +1,131 @@
+NEIS = [
+  [0, -1],
+  [0, 1],
+  [-1, 0],
+  [1, 0],
+]
+
+@start = nil
+@stop = nil
+@board = []
+
+File.readlines('in.txt', chomp: true).each_with_index do |line, y|
+  line.each_char.with_index do |c, x|
+    if c == 'S'
+      @start = [y, x]
+    elsif c == 'E'
+      @stop = [y, x]
+    end
+  end
+  @board << line
+end
+
+def get_distance
+  steps = 1
+
+  q = [@stop]
+  dists = {}
+  dists[@stop] = 0
+  while q.any?
+    q.size.times do
+      y, x = q.shift
+
+      NEIS.each do |dy, dx|
+        ny = y + dy
+        nx = x + dx
+
+        next if @board[ny][nx] == '#'
+        key = [ny, nx]
+        if ny == @start[0] && nx == @start[1]
+          dists[key] = steps
+          break
+        end
+        next if dists.include?(key)
+        dists[key] = steps
+        q << key
+      end
+    end
+    steps += 1
+  end
+
+  dists
+end
+
+def generate_deltas(upto)
+  res = [NEIS]
+  seen = Set.new(NEIS)
+  (upto - 1).times do
+    tmp = []
+    res.last.each do |y, x|
+      NEIS.each do |dy, dx|
+        ny = y + dy
+        nx = x + dx
+        key = [ny, nx]
+        next unless seen.add?([ny, nx])
+        tmp << key
+      end
+    end
+    res << tmp
+  end
+  res
+end
+
+def with_cheating_of_at_most_cheats(cheats, saved)
+  len = @board.size
+  wid = @board[0].size
+
+  dists = get_distance
+  start_dist = dists[@start]
+
+  q = [@start]
+  visited = Set.new(q)
+  steps = 0
+
+  deltas = generate_deltas(cheats)
+
+  count = 0
+
+  while steps <= start_dist - saved
+    q.size.times do
+      y, x = q.shift
+
+      1.upto(deltas.size - 1) do |delta_idx|
+        cost = delta_idx + 1
+        break if steps + cost > start_dist - saved
+        deltas[delta_idx].each do |dy, dx|
+          ny = y + dy
+          nx = x + dx
+
+          next if ny < 0 || ny >= len
+          next if nx < 0 || nx >= wid
+          if (dists[[ny, nx]] || 1.0 / 0) + steps + cost <= start_dist - saved
+            count += 1
+          end
+        end
+      end
+
+      NEIS.each do |dy, dx|
+        ny = y + dy
+        nx = x + dx
+
+        next if @board[ny][nx] == '#'
+        key = [ny, nx]
+        next unless visited.add?(key)
+        q << key
+      end
+    end
+    steps += 1
+  end
+  count
+end
+
+def part1
+  with_cheating_of_at_most_cheats(2, 100)
+end
+
+def part2
+  with_cheating_of_at_most_cheats(20, 100)
+end
+
+p part1
+p part2
